3750d3ebab7d7792f75012229db9f3979b63c772
Eddie Antonio Santos <easantos@ualberta.ca>
2019-08-27 12:07:14 -0600

    Start documentation on exporting from various apps.

---
+++ developer/12.0/guides/lexical-models/export/index.php
@@ -0,0 +1,12 @@
+<?php
+  require_once('includes/template.php');
+
+  head([
+    'title' => "How to export a word list from various software",
+    'css' => ['template.css', 'index.css', 'prism.css', 'dev-lexical-models.css']
+  ]);
+?>
+
+<h1 class="title">How to export a word list from various software</h1>
+
+
